66ab8117a5fa8264797250d15e1fbf0dbbe79b82
Aaron Portnoy <aportnoy@gmail.com>
2024-12-13 11:00:46 -0500

    changed temp dir code

--- app/findbin.py
+++ app/findbin.py
@@ -11,15 +11,16 @@ def main(args):
     for tar_file in glob.glob(f"{args.input_path}/*"):
         print(tar_file)
         #Untar file in temp
-        result = subprocess.run(["tar", "-xf", tar_file, "-C", f"{args.temp_dir}"], capture_output=True, text=True)
+        temp = args.temp_dir
+        result = subprocess.run(["tar", "-xf", tar_file, "-C", f"{temp}"], capture_output=True, text=True)
 
         #Depackage all the debs out of the directory to temp
-        for deb_file in glob.glob("/temp/**/*.deb", recursive = True):
-            result = subprocess.run(["dpkg-deb", "-x", deb_file, "/temp"], capture_output=True, text=True)
+        for deb_file in glob.glob(f"{temp}/**/*.deb", recursive = True):
+            result = subprocess.run(["dpkg-deb", "-x", deb_file, temp], capture_output=True, text=True)
 
         elf_files = []
         #File all files in directory and extract all the executables
-        for each_file in glob.glob('/temp/**', recursive = True):
+        for each_file in glob.glob(f"{temp}/**", recursive = True):
             result = subprocess.run(["file", each_file], capture_output=True, text=True)
             print(result)
             #Get the stdout of the process call 
@@ -40,13 +41,13 @@ def main(args):
             shutil.copy(elf, os.path.join(output_directory, os.path.basename(elf)))
 
         #Clean up directory 
-        subprocess.run(f'rm -rf {args.temp_dir}/*', shell=True)
+        subprocess.run(f'rm -r {temp}/*', shell=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find executable ELFs')
     parser.add_argument("--input_path", type=str, default = "/input_data", help="Input path")
     parser.add_argument("--output_path", type=str, default = "/output_data", help="Output path")
-    parser.add_argument("--temp_dir", type=str, default = "/temp", help="Output path")
+    parser.add_argument("--temp_dir", type=str, default="./tmp", help="Output path")
 
     args = parser.parse_args()
     main(args)
